5a61f0ac4f8ddfbfdaed8ae42dc6dcdc34c6f74e
Daniela DDavila <utm22030607@utma.edu.mx>
2023-05-31 16:01:27 -0600

    feat: a tuple was added

--- exercises.py
+++ exercises.py
@@ -2,3 +2,10 @@ lista= [1,2,3,4,5]
 lista[4]=input("ingrese el valor que se almacenara en la lista ")
 for i in lista: 
     print (i)
+
+nums= (1,2,3,4,5,6,7)
+i=0
+while i< len (nums):
+    print(nums[i])
+    i= i+1
+    
